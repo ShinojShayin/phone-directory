f6dbc90cb978b1e31a3c514a9b210f4c06a1b3ca
Shinoj <ShinojShayin@users.noreply.github.com>
2021-03-28 21:40:35 +0530

    Components: Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header.js";
 
 function App() {
-  return <div>Phone directory</div>;
+  return (
+    <div>
+      <Header />
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <br />
+        <span>Phone</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = function () {
+  return <div className="header">Phone Directory</div>;
+};
+
+// import React, {Component} from 'react';
+
+// class MyComponent extends Component {
+//    render() {
+//     return (
+//         // code here
+//      )
+//    }
+// }
+
+export default Header;

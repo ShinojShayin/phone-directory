628c9addc6797fee6583d6c44dff432eb3f2dbfd
Shinoj <ShinojShayin@users.noreply.github.com>
2021-04-04 20:52:49 +0530

    Added ‘PhoneDirectory’ as smart component, renamed ‘App’ as ‘ShowSubscribers’ and converted ‘AddSubscribers’ and ‘ShowSubscribers’ as dumb components

---
+++ src/PhoneDirectory.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import AddSubscriber from "./AddSubscriber";
+import ShowSubscribers from "./ShowSubscribers";
+
+class PhoneDirectory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersList: [
+        {
+          id: 1,
+          name: "Shilpa Bhat",
+          phone: "88888888888888",
+        },
+        {
+          id: 2,
+          name: "Srishti Gupta",
+          phone: "999999999",
+        },
+      ],
+    };
+  }
+
+  addSubscriberHandler = (newSubscriber) => {
+    let subscribersList = this.state.subscribersList;
+
+    if (subscribersList.length > 0) {
+      newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+    } else {
+      newSubscriber.id = 1;
+    }
+    subscribersList.push(newSubscriber);
+    this.setState({ subscribersList: subscribersList });
+    console.log(this.state.subscribersList);
+  };
+
+  render() {
+    // return <AddSubscriber addSubscriberHandler={this.addSubscriberHandler} />;
+    return <ShowSubscribers subscribersList={this.state.subscribersList} />;
+  }
+}
+
+export default PhoneDirectory;
